bd553069239234dfcf81914fff125fca9df63d60
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-23 05:22:21 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,5 +1,5 @@
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+requires 'pry'
 
 def hello(array)
   i = 0
@@ -8,6 +8,6 @@ def hello(array)
     i += 1
   end
 end
-
-
+binding.pry
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
